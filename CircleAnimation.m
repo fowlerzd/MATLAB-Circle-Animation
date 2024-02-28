7c0fce29bca26409d79ef5241236f64accd1bdbc
zdfowler0 <155563410+zdfowler0@users.noreply.github.com>
2024-02-28 13:29:59 -0500

    Update CircleAnimation.m
    Animation now goes through 1 point to 10

--- CircleAnimation.m
+++ CircleAnimation.m
@@ -1,31 +1,33 @@
-num_points = 2;
-
-offset = 1:num_points;
 frames = 1:64;
-
-spacing = num_points/2;
-
-% make time sets with offsets
-for i = offset
-    t(i, frames) = linspace(-(offset(i)*pi/spacing), (2*pi) - (offset(i)*pi/spacing), 64);
-end
-
-% generate functions
-for i = offset
-    x(i, frames) = cos(t(i, frames));
-    y(i, frames) = sin(t(i, frames));
-end
-
-colors = ["red", "black"];
-
-% generate points
-for i = frames
-    for ii = offset
-    scatter(x(ii,i), y(ii,i), 40, colors(mod(i,2)+1), "o", "filled")
-    hold("on")
+for points = 1:10
+    num_points = points;
+    
+    offset = 1:num_points;
+    
+    spacing = num_points/2;
+    
+    % make time sets with offsets
+    for i = offset
+        t(i, frames) = linspace(-(offset(i)*pi/spacing), (2*pi) - (offset(i)*pi/spacing), size(frames,2));
     end
-
-    axis([-10 10 -10 10])
-    pause(0.0000001)
-    hold("off")
+    
+    % generate functions
+    for i = offset
+        x(i, frames) = cos(t(i, frames));
+        y(i, frames) = sin(t(i, frames));
+    end
+    
+    colors = ["red", "black"];
+    
+    % generate points
+    for i = frames
+        for ii = offset
+        scatter(x(ii,i), y(ii,i), 40, colors(mod(i,2)+1), "o", "filled")
+        hold("on")
+        end
+    
+        axis([-10 10 -10 10])
+        pause(0.0000001)
+        hold("off")
+    end
 end
